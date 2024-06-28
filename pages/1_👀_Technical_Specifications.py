47f7c7058148c2a04a8f9a5a17071b223578cc7c
Huet Dastarac <Margerie@users.noreply.huggingface.co>
2024-06-28 09:33:23 +0000

    Upload 3 files

---
+++ pages/1_👀_Technical_Specifications.py
@@ -0,0 +1,61 @@
+import streamlit as st
+from persist import persist, load_widget_state
+import json
+import requests
+#from specific_extraction import extract_it
+
+
+# global variable_output
+
+
+
+def get_cached_data():
+    # json.load(open('file_TG263.json'))
+    struct_dict = {"Target":["GTV","CTV","PTV"],"Anatomy":["SpinalCord","BrainStem"]}
+    r = requests.get('https://huggingface.co/api/models-tags-by-type')
+    tags_data = r.json()
+    libraries = [x['id'] for x in tags_data['library']]
+    return struct_dict, libraries
+
+def main():
+    cs_body()
+   
+
+
+def cs_body():
+    
+    struct_dict, libraries = get_cached_data()
+    st.header('Technical Specifications')
+    st.write("Provide an overview of any additional technical specifications for this model")
+    st.markdown('##### Model architecture')
+    st.number_input("Total number of trainable parameters [million]",value=5)
+    left, middle, right = st.columns(3)
+    nlines = int(left.number_input("Input channels", 0, 20, 1))
+    for i in range(nlines):
+        type_input = middle.selectbox(f"Input type # {i}", list(struct_dict.keys()))
+        right.selectbox("Input",struct_dict[type_input], help="From https://aapm.onlinelibrary.wiley.com/doi/pdf/10.1002/acm2.12701")
+    st.text_input("Loss function",placeholder="MSE")
+    st.number_input("Batch size",value=1)
+    left, right = st.columns(2)
+    nlines = int(left.number_input("Patch dimension", 2, 3, 3))
+    # cols = st.columns(ncol)
+    for i in range(nlines):
+        right.number_input(f"Dim [px] # {i}", key=i,value=128)
+    arch_fig = st.file_uploader("Figure of the architecture",type=['png','jpg'])
+    if arch_fig is not None:
+        st.image(arch_fig)
+    
+    st.selectbox("Library/Dependencies", libraries,help="The name of the library this model came from (Ex. pytorch, timm, spacy, keras, etc.). This is usually automatically detected in model repos, so it is not required.", key=persist('library_name'))
+    st.text_input("Hardware recommended", placeholder="GPU 20Gb RAM", key=persist("Model_hardware"))
+    st.number_input("Inference time for recommended hardware [seconds]",value=10, key=persist("inference_time"))
+    st.text_area("Installation / Getting started", placeholder="Installation procedure / code to run inference", key=persist("Model_how_to"))
+
+
+        
+   
+    
+    
+
+if __name__ == '__main__':
+    load_widget_state()
+    main()
---
+++ pages/3_🔬_Model_Evaluation.py
@@ -0,0 +1,66 @@
+import streamlit as st
+from persist import persist, load_widget_state
+from pathlib import Path
+
+from middleMan import apply_view,writingPrompt
+
+global variable_output
+
+def main():
+    cs_body()
+    
+
+def cs_body():
+  
+    #stateVariable = 'Model_Eval'
+    #help_text ='Detail the Evaluation Results for this model'
+    #col1.header('Model Evaluation')
+    st.markdown('# Evaluation')
+    st.text_area(" This section describes the evaluation protocols and provides the results. ",help="Detail the Evaluation Results for this model")
+    st.markdown('## Testing Data, Factors & Metrics:')
+    left, right = st.columns([2,4])
+    
+    #st.markdown('### Model Description')
+    
+
+    with left: 
+        st.write("\n")
+        st.write("\n")
+        st.markdown('#### Testing Data:')
+        st.write("\n")
+        st.write("\n")
+        st.write("\n")
+        st.write("\n")
+        st.write("\n")
+        st.write("\n")
+        #st.write("\n")
+        st.markdown('#### Factors:')
+        st.write("\n")
+        st.write("\n")
+        st.write("\n")
+        st.write("\n")
+        st.write("\n")
+        st.write("\n")
+        st.markdown('#### Metrics:')
+        st.write("\n")
+        st.write("\n")
+        st.write("\n")
+        st.write("\n")
+        st.write("\n")
+        st.markdown('#### Results:')
+        
+    with right:
+        #soutput_jinja = parse_into_jinja_markdown()
+        st.text_area("", help="Ideally this links to a Dataset Card.",key=persist("Testing_Data"))
+        #st.write("\n")
+        st.text_area("",help="What are the foreseeable characteristics that will influence how the model behaves? This includes domain and context, as well as population subgroups.",key=persist("Factors"))
+        st.text_area("", help="What metrics will be used for evaluation in light of tradeoffs between different errors?", key=persist("Metrics"))
+        st.text_area("", key=persist("Model_Results"))
+
+   
+    
+    
+
+if __name__ == '__main__':
+    load_widget_state()
+    main()
